8d7e9754f77fb8cf98c747e9067e2ed23076cf6d
zhourgong <zhougong@weilan.com>
2023-11-24 09:48:04 +0800

    [feature]: fix iOS build.

--- packages/wifi_iot/ios/Classes/WifiIotPlugin.m
+++ packages/wifi_iot/ios/Classes/WifiIotPlugin.m
@@ -1,5 +1,12 @@
 #import "WifiIotPlugin.h"
+#if __has_include(<wifi_iot/wifi_iot-Swift.h>)
 #import <wifi_iot/wifi_iot-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "wifi_iot-Swift.h"
+#endif
 
 @implementation WifiIotPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
